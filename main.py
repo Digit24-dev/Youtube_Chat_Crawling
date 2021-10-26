08e810a230fc42c810b3d161a519dc19e149355c
Digit24-dev <57099725+Digit24-dev@users.noreply.github.com>
2021-10-26 21:54:54 +0900

    V_1.0.2 alpha version build.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from builtins import print, KeyboardInterrupt, type, set, len, list
+from builtins import print, KeyboardInterrupt, type, set, len, list, str
 
 import pytchat #import LiveChat
 import pafy
 import pandas as pd
 import random
+import time
 
 # 유튜브 API V_3 의 api 키 받아서 입력
-pafy.set_api_key('')
+pafy.set_api_key('AIzaSyCYq4WSuf4jjYyV6nxHVpBfASzfQeA6niw')
 
 # 유튜브 라이브 주소를 입력해 주세요.
 # 예를 들어, 다음과 같은 주소가 있을 때에
@@ -17,6 +18,7 @@ video_id = 'GoXPbGQl-uQ'
 
 id_list = set("")
 start_flag = False
+cast_lot = False
 
 v = pafy.new(video_id)
 title = v.title
@@ -29,37 +31,62 @@ empty_frame.to_csv('./youtube.csv', encoding="UTF-8")
 chat = pytchat.create(video_id)           # topchat_only='FALSE'
 
 while chat.is_alive():
+
+    if cast_lot:
+        break
+
     try:
         data = chat.get()
         items = data.items
         for c in items:
-
             if c.author.name == "[오제이 튜브]OJ Tube":
                 if c.message == "끝!" :
                     start_flag = False
                 elif c.message == "시작!" :
                     start_flag = True
 
+                if c.message == "추첨!" :
+                    cast_lot = True
+
             if start_flag:
                 id_list.add(c.author.name)
 
             print(f"{c.datetime} [{c.author.name}] - {c.message}")
 
-            if len(id_list) > 10:
-                print(">>>>>>>" + random.choice(list(id_list)) + "<<<<<<<<")
-
             # time.sleep(3)
             data.sync_items()
-            data2 = {'제목' : [title], '채널 명' : [author], '스트리밍 시작 시간' : [published], '댓글 작성자' : [c.author.name], '댓글 내용' : [c.datetime], '댓글 작성 시간' : [c.message]}
-            result = pd.DataFrame(data2)
-
+            # data2 = {'제목' : [title], '채널 명' : [author], '스트리밍 시작 시간' : [published], '댓글 작성자' : [c.author.name], '댓글 내용' : [c.datetime], '댓글 작성 시간' : [c.message]}
+            # result = pd.DataFrame(data2)
             # result.to_csv('youtube.csv', mode='a', header=False, encoding='UTF-8')
+
     except KeyboardInterrupt:
         chat.terminate()
         print(id_list)
         break
 
+print("모집 끝! 현재까지 " + str(len(id_list)) + "명의 인원이 모였습니다!")
+time.sleep(2)
+print("추첨을 시작합니다!")
 
+time.sleep(1)
+print("3!!!")
 
+time.sleep(1)
+print("2!!!")
+
+time.sleep(1)
+print("1!!!")
+print()
+
+time.sleep(1)
+print("당첨자는~!?")
+print()
+
+time.sleep(3)
+print(">>>>>>>   " + random.choice(list(id_list)) + " 님!  <<<<<<<<")
+print("축하합니다!!!!!")
+
+time.sleep(5)
+print("프로그램을 종료합니다.")
 
 
